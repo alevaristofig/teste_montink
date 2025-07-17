5e308db912a08079cbe71c500252aa64ce156e66
Alexandre <alevaristofig@gmail.com>
2025-07-17 14:49:04 -0300

    Criação do controller produto

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ProdutoRequest;
+use App\Service\ProdutoService;
+use Illuminate\Http\JsonResponse;
+
+class ProdutoController extends Controller
+{
+    private $service;
+
+    public function __construct(ProdutoService $service) {
+        $this->service = $service;
+    }
+}
