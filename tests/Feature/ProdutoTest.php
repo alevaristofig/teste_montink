c27abe964aabdc8ec619ca4d427533722d3824cd
Alexandre <alevaristofig@gmail.com>
2025-07-17 14:04:15 -0300

    Refatoração no teste de insert de produtos, acrescentou a inserção de estoque

--- tests/Feature/ProdutoTest.php
+++ tests/Feature/ProdutoTest.php
@@ -7,26 +7,43 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Mockery;
 use App\Models\Produto;
+use App\Models\Estoque;
 
 class ProdutoTest extends TestCase
 {
-    public function test_InserirProdutoSucesso(): void {
+    public function test_InserirProdutoEEstoqueSucesso(): void {
 
         $dados = [
             "nome" => "Produto Teste",
             "preco" => "100",
             "variacoes" => "teste",
+            "estoque" => [
+                "id" => 1,
+                "id_produto" => 1,
+                "quantidade" => 10
+            ]
         ];
 
-        $mock = Mockery::mock('alias:' . Produto::class);
-        $mock->shouldReceive('create')
+        $mockProduto = Mockery::mock('alias:' . Produto::class);
+        $mockEstoque = Mockery::mock('alias:' . Estoque::class);
+
+        $mockProduto->shouldReceive('create')
             ->once()
             ->with($dados)
             ->andReturn((object) $dados);
 
-        $result = Produto::create($dados);
+        $mockEstoque->shouldReceive('create')
+            ->once()
+            ->with($dados['estoque'])
+            ->andReturn((object) $dados['estoque']);
 
-        $this->assertEquals("Produto Teste",$result->nome);
-        $this->assertEquals(100,$result->preco);
+        $resultProduto = Produto::create($dados);
+        $resultEstoque = Estoque::create($dados['estoque']);
+
+        $this->assertEquals("Produto Teste",$resultProduto->nome);
+        $this->assertEquals(100,$resultProduto->preco);
+
+        $this->assertEquals(1,$resultEstoque->id_produto);
+        $this->assertEquals(10,$resultEstoque->quantidade);
     }
 }
