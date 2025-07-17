a132172c2004a6125f916a8b525f1cdcb18932f3
Alexandre <alevaristofig@gmail.com>
2025-07-17 12:14:06 -0300

    Criação do teste para inserir o produto

--- tests/Feature/ProdutoTest.php
+++ tests/Feature/ProdutoTest.php
@@ -5,16 +5,28 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Mockery;
+use App\Models\Produto;
 
 class ProdutoTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    public function test_InserirProdutoSucesso(): void {
 
-        $response->assertStatus(200);
+        $dados = [
+            "nome" => "Produto Teste",
+            "preco" => "100",
+            "variacoes" => "teste",
+        ];
+
+        $mock = Mockery::mock('alias:' . Produto::class);
+        $mock->shouldReceive('create')
+            ->once()
+            ->with($dados)
+            ->andReturn((object) $dados);
+
+        $result = Produto::create($dados);
+
+        $this->assertEquals("Produto Teste",$result->nome);
+        $this->assertEquals(100,$result->preco);
     }
 }
