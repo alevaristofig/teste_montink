cb28f4baee82fb4137dd869a6e9f25b1d952bcb6
Alexandre <alevaristofig@gmail.com>
2025-07-17 19:17:38 -0300

    Criação do controller cupom

---
+++ app/Http/Controllers/CupomController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CupomRequest;
+use App\Service\CupomService;
+use Illuminate\Http\JsonResponse;
+
+class CupomController extends Controller
+{
+    private $service;
+
+    public function __construct(CupomService $service) {
+        $this->service = $service;
+    }
+
+    public function salvar(CupomRequest $request): JsonResponse
+    {
+        $result = $this->service->salvar($request);
+
+       return $result;
+    }
+}
