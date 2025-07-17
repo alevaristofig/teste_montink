81ccd44152e0ad2499752b5f427188e1b9d3918c
Alexandre <alevaristofig@gmail.com>
2025-07-17 18:16:20 -0300

    Criação do request de cupom para validação dos dados

---
+++ app/Repository/CupomRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Repository;
+
+    use App\Http\Requests\CupomRequest;
+    use App\Models\Cupom;
+    use Illuminate\Http\JsonResponse;
+    use Illuminate\Database\Eloquent\Collection;
+
+    interface CupomRepository {
+        
+        public function salvar(CupomRequest $request): JsonResponse;
+        /*public function listar(): Collection;
+        public function buscar(int $id): Produtos;
+        public function atualizar(int $id, ProdutoRequest $request): Produtos;
+        public function deletar(int $id): void;*/
+    }
