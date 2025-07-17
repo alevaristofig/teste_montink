963dbf318f5d62690be20f500d42f16f0cda2568
Alexandre <alevaristofig@gmail.com>
2025-07-17 18:19:49 -0300

    Criação do repositorio e service do cupom

---
+++ app/Service/CupomService.php
@@ -0,0 +1,30 @@
+<?php
+
+    namespace App\Service;
+
+    use App\Repository\CupomRepository;
+    use App\Http\Requests\CupomRequest;
+    use App\Models\Cupom;
+    use Illuminate\Http\JsonResponse;
+    use Illuminate\Database\Eloquent\Collection;
+    use App\Exceptions\ApiMessages;
+
+    class EstoqueService implements CupomRepository {
+
+        private $model;
+
+        public function __construct(Cupom $model) {
+            $this->model = $model;
+        }
+
+        public function salvar(CupomRequest $dados): JsonResponse {
+             try {                                            
+                $cupom = $this->model->create($dados);   
+                
+                return response()->json($cupom, 201);
+            } catch(\Exception $e) {
+                $message = new ApiMessages("Ocorreu um erro, a operção não foi realizada",$e->getMessage());
+                response()->json(['error' => $message->getMessage()], 500);
+            }
+        }
+    }
