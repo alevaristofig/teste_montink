a032c87f3d93109fa2e55f4644666f9ab0afef46
Alexandre <alevaristofig@gmail.com>
2025-07-17 23:57:23 -0300

    Criação do request de pedidos, para a validação de dados

---
+++ app/Http/Requests/PedidoRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PedidoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "id_user" => 'required',
+            "produto_id" => 'required',
+            "quantidade" => 'required|numeric',
+            "data" => 'required',            
+            "valor_total" => 'required',
+            "status" => 'required'
+        ];
+    }
+}
