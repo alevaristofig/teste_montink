f72784d0381511eec98db5a91db83c47d7f893ad
Alexandre <alevaristofig@gmail.com>
2025-07-18 01:11:11 -0300

    Correção nos metodos realizarPedido, formatar os dados para grave como um array de json e listarPedidos, exibir os dados como um array

--- app/Service/PedidoService.php
+++ app/Service/PedidoService.php
@@ -26,32 +26,37 @@
         {
             try {
                     $carrinho = Redis::hgetall($this->nomeCarrinho); 
-                   
+                  
                     if (count($carrinho) == 0) 
                     {
                         return response()->json([], 200);
                     }  
                 
-                    return response()->json($carrinho, 200);
+                    return response()->json(json_decode($carrinho['pedido'],true), 200);
             } catch(\Exception $e) {
                 $message = new ApiMessages("Ocorreu um erro, a operção não foi realizada",$e->getMessage());
-                response()->json(['error' => $message->getMessage()], 500);
+                return response()->json(['error' => $message->getMessage()], 500);
             }
         }
 
         public function realizarPedidos(PedidoRequest $dados): JsonResponse
         {
             try {
-                    $carrinho = '';
+                    $carrinho = [];
                     $pedido = Redis::hgetall($this->nomeCarrinho); 
                     $dados = $dados->all();
                     $dados['valor_total'] = $dados['valor_total'] * $dados['quantidade'];
 
                     if(count($pedido) === 0) 
-                    {    
-                        $carrinho = $dados;                      
-                    } else {
-                        dd('nao');
+                    {   
+                        $dados = [$dados];
+                        $carrinho['pedido'] = json_encode($dados);                      
+                    } else {                        
+                        $produtos = Redis::hget($this->nomeCarrinho,'pedido');                         
+                        $produtos = json_decode($produtos,true);                        
+                        $i = array_key_last($produtos);                       
+                        $produtos[$i+1] = $dados;                       
+                        $carrinho['pedido'] = json_encode($produtos);  
                     }
 
                     Redis::hmset($this->nomeCarrinho,$carrinho);
